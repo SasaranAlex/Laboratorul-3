2dabdf4bdf5787c823955dfaa92fa7bdecba7320
SasaranAlex <77351720+SasaranAlex@users.noreply.github.com>
2021-01-12 20:19:31 +0200

    Delete delete.php

--- delete.php
+++
@@ -1,9 +0,0 @@
-<?php session_start();
-include('config.php'); 
-$mytable = $_SESSION["mytable"]; 
-
-$id = (int) $_GET['id']; 
-$dbi->query("DELETE FROM " . $mytable . " WHERE id = '$id'") ; 
-
-echo '<script type="text/javascript">window.location="list.php";</script>';
-?> 
