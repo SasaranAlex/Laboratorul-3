8b587beed78066ee22b28b3f4e74fa9a45d4d9c2
SasaranAlex <77351720+SasaranAlex@users.noreply.github.com>
2021-01-12 20:19:23 +0200

    Delete config.php

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-$my_database = "testwork";
-
-$dbi = new mysqli('phpCRUD', 'vb', '123456', $my_database);
-if($dbi->connect_errno) die("Connect failed: " . $dbi->connect_error); 
-
-// echo $dbi->character_set_name();
-$dbi->set_charset("utf8");
-?> 
